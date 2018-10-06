ea75a3b935625571ce070117f66ca79dc6a0453d
Nathan Bolser <nbolser@gmail.com>
2018-10-06 10:14:08 -0600

    Component changes

--- app/index.js
+++ app/index.js
@@ -4,7 +4,11 @@ require("./index.css");
 
 class App extends React.Component {
   render() {
-    return <div>Hello World!</div>;
+    return (
+      <div>
+        Hello Bart!
+      </div>
+    );
   }
 }
 
