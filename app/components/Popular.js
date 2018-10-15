5c751b49254470b9cc0017d3c784d57084d8b2cf
Nathan Bolser <nathan.bolser@travelershaven.com>
2018-10-15 11:16:30 -0600

    Add popular repos API call

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,57 +1,78 @@
 var React = require("react");
-var PropTypes = require('prop-types');
+var PropTypes = require("prop-types");
+var api = require("../utils/api");
 
 function SelectLanguage(props) {
-  var languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python'];
+  var languages = ["All", "Javascript", "Ruby", "Java", "CSS", "Python"];
 
-    return(
-      <ul className='languages'>
-      { languages.map( function(language) {
-        return ( 
+  return (
+    <ul className="languages">
+      {languages.map(function(language) {
+        return (
           <li
-            style={language === props.selectedLanguage ? { color: '#d0021b'} : null}
+            style={
+              language === props.selectedLanguage ? { color: "#d0021b" } : null
+            }
             onClick={props.onSelect.bind(null, language)}
-            key={language}> 
-            { language }
+            key={language}
+          >
+            {language}
           </li>
-        )
+        );
       }, this)}
     </ul>
-    );
+  );
 }
 
 SelectLanguage.propTypes = {
   selectedLanguage: PropTypes.string.isRequired,
   onSelect: PropTypes.func.isRequired
-}
+};
 
 class Popular extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
     this.state = {
-      selectedLanguage: 'All'
+      selectedLanguage: "All",
+      repos: null
     };
     this.updateLanguage = this.updateLanguage.bind(this);
   }
 
+  componentDidMount() {
+    this.updateLanguage(this.state.selectedLanguage);
+  }
+
   updateLanguage(language) {
-    this.setState(function(){
+    this.setState(function() {
       return {
-        selectedLanguage: language
-      }
+        selectedLanguage: language,
+        repos: null
+      };
     });
+
+    api.fetchPopularRepos(language).then(
+      function(repos) {
+        this.setState(function() {
+          return {
+            repos: repos
+          };
+        });
+      }.bind(this)
+    );
   }
 
   render() {
     return (
       <div>
         <SelectLanguage
-            selectedLanguage={this.state.selectedLanguage}
-            onSelect={this.updateLanguage}
+          selectedLanguage={this.state.selectedLanguage}
+          onSelect={this.updateLanguage}
         />
+        {JSON.stringify(this.state.repos, null, 2)}
       </div>
     );
-  };
-};
+  }
+}
 
 module.exports = Popular;
