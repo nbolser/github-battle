b46917850273d9f924ae33800eba12f9850d77d3
Nathan Bolser <nathan.bolser@travelershaven.com>
2018-11-14 07:50:30 -0700

    Add Loading component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,6 +1,7 @@
 var React = require("react");
 var PropTypes = require("prop-types");
 var api = require("../utils/api");
+var Loading = require("./Loading");
 
 function SelectLanguage(props) {
   var languages = ["All", "Javascript", "Ruby", "Java", "CSS", "Python"];
@@ -103,7 +104,7 @@ class Popular extends React.Component {
           onSelect={this.updateLanguage}
         />
         {!this.state.repos ? (
-          <p>Fetching data...</p>
+          <Loading />
         ) : (
           <RepoGrid repos={this.state.repos} />
         )}
--- app/components/Results.js
+++ app/components/Results.js
@@ -1,48 +1,51 @@
 var React = require("react");
-var queryString = require('query-string');
-var PropTypes = require('prop-types');
-var api = require('../utils/api');
+var queryString = require("query-string");
+var PropTypes = require("prop-types");
+var api = require("../utils/api");
 var Link = require("react-router-dom").Link;
-var PlayerPreview = require('./PlayerPreview');
+var PlayerPreview = require("./PlayerPreview");
+var Loading = require("./Loading");
 
-function Profile (props) {
+function Profile(props) {
   var info = props.info;
 
   return (
     <PlayerPreview avatar={info.avatar_url} username={info.login}>
-      <ul className='space-list-item'>
+      <ul className="space-list-item">
         {info.name && <li>{info.name}</li>}
         {info.location && <li>{info.location}</li>}
         {info.company && <li>{info.company}</li>}
         <li>Followers: {info.followers}</li>
         <li>Following: {info.following}</li>
         <li>Public Repos: {info.public_repos}</li>
-        {info.blog && <li><a href={info.blog}>{info.blog}</a></li>}
+        {info.blog && (
+          <li>
+            <a href={info.blog}>{info.blog}</a>
+          </li>
+        )}
       </ul>
     </PlayerPreview>
-  )
+  );
 }
 
-function Player (props) {
+function Player(props) {
   return (
     <div>
-      <h1 className='header'>{ props.label }</h1>
-      <h3 style={{textAlign: 'center'}}>Score: { props.score }</h3>
-      <Profile info={props.profile}>
-
-      </Profile>
+      <h1 className="header">{props.label}</h1>
+      <h3 style={{ textAlign: "center" }}>Score: {props.score}</h3>
+      <Profile info={props.profile} />
     </div>
-  )
+  );
 }
 
 Player.propTypes = {
   label: PropTypes.string.isRequired,
   score: PropTypes.number.isRequired,
   profile: PropTypes.object.isRequired
-}
+};
 
 class Results extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
 
     this.state = {
@@ -50,37 +53,34 @@ class Results extends React.Component {
       loser: null,
       error: null,
       loading: true
-    }
+    };
   }
 
   componentDidMount() {
     var players = queryString.parse(this.props.location.search);
-    api.battle([
-      players.playerOne,
-      players.playerTwo
-    ])
-    .then( function(results) {
-      if (results === null) {
-        return this.setState(function() {
-          return {
-            error: 'Looks like something went wrong.',
-            loading: false
-          }
-        })
-      };
-
-      this.setState( function() {
-        return {
-          error: null,
-          winner: results[0],
-          loser: results[1],
-          loading: false
+    api.battle([players.playerOne, players.playerTwo]).then(
+      function(results) {
+        if (results === null) {
+          return this.setState(function() {
+            return {
+              error: "Looks like something went wrong.",
+              loading: false
+            };
+          });
         }
-      });
-    }.bind(this));
+
+        this.setState(function() {
+          return {
+            error: null,
+            winner: results[0],
+            loser: results[1],
+            loading: false
+          };
+        });
+      }.bind(this)
+    );
   }
-      
-      
+
   render() {
     var winner = this.state.winner;
     var loser = this.state.loser;
@@ -88,33 +88,25 @@ class Results extends React.Component {
     var loading = this.state.loading;
 
     if (loading === true) {
-      return <p>Loading...</p>
-    };
+      <Loading text="Downloading" />;
+    }
 
     if (error) {
       return (
         <div>
-          <p>{ error }</p>
-          <Link to='/battle'>Reset</Link>
+          <p>{error}</p>
+          <Link to="/battle">Reset</Link>
         </div>
-      )
+      );
     }
-    
+
     return (
       <div>
-        <Player
-          label='Winner'
-          score={winner.score} 
-          profile={winner.profile}
-        />
-        <Player
-          label='Loser'
-          score={loser.score} 
-          profile={loser.profile}
-        />
+        <Player label="Winner" score={winner.score} profile={winner.profile} />
+        <Player label="Loser" score={loser.score} profile={loser.profile} />
       </div>
-    )
+    );
   }
-};
+}
 
 module.exports = Results;
